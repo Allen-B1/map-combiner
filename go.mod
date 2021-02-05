aa246d70f543cab3a75097a0e616c37bad627ae8
Allen B <pingguo.ny@gmail.com>
2021-02-04 22:03:30 -0500

    stuff

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/allen-b1/ns-map-combiner
+
+go 1.15
--- main.go
+++ main.go
@@ -3,13 +3,38 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/color"
 	"image/draw"
 	"image/png"
+	"log"
 	"math/rand"
+	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
+
+func ParseHexColor(s string) (clr color.Color, err error) {
+	c := color.RGBA{}
+	c.A = 0xff
+	switch len(s) {
+	case 7:
+		_, err = fmt.Sscanf(s, "#%02x%02x%02x", &c.R, &c.G, &c.B)
+	case 4:
+		_, err = fmt.Sscanf(s, "#%1x%1x%1x", &c.R, &c.G, &c.B)
+		// Double the hex digits:
+		c.R *= 17
+		c.G *= 17
+		c.B *= 17
+	default:
+		err = fmt.Errorf("invalid length, must be 7 or 4")
+	}
+	clr = c
+	return
+}
 
 func img2rgba(in image.Image) *image.RGBA {
 	b := in.Bounds()
@@ -18,53 +43,98 @@ func img2rgba(in image.Image) *image.RGBA {
 	return dst
 }
 
-func main() {
-	rand.Seed(time.Now().UnixNano())
-
-	f, err := os.Open(os.Args[1])
-	if err != nil {
-		panic(err)
+func combine(bg color.Color, imgs ...image.Image) image.Image {
+	if len(imgs) == 0 {
+		return nil
 	}
-	img1, err := png.Decode(f)
-	if err != nil {
-		panic(err)
+	if len(imgs) == 1 {
+		return imgs[0]
 	}
 
-	dst := img2rgba(img1)
+	bgR, bgG, bgB, _ := bg.RGBA()
 
-	f, err = os.Open(os.Args[2])
-	if err != nil {
-		panic(err)
-	}
-	img2, err := png.Decode(f)
-	if err != nil {
-		panic(err)
-	}
+	dst := img2rgba(imgs[0])
 
-	img2 = img2rgba(img2)
-
-	b := dst.Bounds()
-	for x := b.Min.X; x < b.Max.X; x++ {
-		for y := b.Min.Y; y < b.Max.Y; y++ {
-			color1 := dst.At(x, y)
-			color2 := img2.At(x, y)
-			r2, g2, b2, a2 := color2.RGBA()
-			if r, g, b, _ := color1.RGBA(); r == 0xffff && g == 0xffff && b == 0xffff && (r2 != 0xffff || b2 != 0xffff || g2 != 0xffff) && a2 != 0 {
-				dst.Set(x, y, color2)
+	for _, img2 := range imgs[1:] {
+		b := dst.Bounds()
+		for x := b.Min.X; x < b.Max.X; x++ {
+			for y := b.Min.Y; y < b.Max.Y; y++ {
+				color1 := dst.At(x, y)
+				color2 := img2.At(x, y)
+				r2, g2, b2, a2 := color2.RGBA()
+				if r, g, b, _ := color1.RGBA(); r == bgR && g == bgG && b == bgB && (r2 != bgR || b2 != bgB || g2 != bgG) && a2 != 0 {
+					dst.Set(x, y, color2)
+				}
 			}
 		}
 	}
 
-	id := strconv.FormatInt(rand.Int63(), 36)
-	f, err = os.Create(id + ".png")
-	if err != nil {
-		panic(err)
-	}
+	return dst
+}
 
-	fmt.Println(id + ".png")
+func main() {
+	rand.Seed(time.Now().UnixNano())
 
-	err = png.Encode(f, dst)
-	if err != nil {
-		panic(err)
-	}
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.File("index.html")
+	})
+
+	r.GET("/api/combine", func(c *gin.Context) {
+		clrstr := c.Query("color")
+		clr, err := ParseHexColor(clrstr)
+		if err != nil {
+			clr = color.White
+		}
+
+		imgs := strings.Split(strings.Replace(c.Query("url"), "\r", "", -1), "\n")
+		imglist := make([]image.Image, 0)
+		for _, imgurl := range imgs {
+			imgurl = strings.TrimSpace(imgurl)
+			if imgurl == "" {
+				continue
+			}
+			resp, err := http.Get(imgurl)
+			if err != nil {
+				c.String(400, "error: %s", err)
+				return
+			}
+			img, _, err := image.Decode(resp.Body)
+			if err != nil {
+				c.String(400, "error: %s", err)
+				return
+			}
+			imglist = append(imglist, img)
+		}
+
+		dest := combine(clr, imglist...)
+		if dest == nil {
+			c.String(400, "no images provided")
+		}
+
+		id := strconv.FormatInt(rand.Int63(), 36)
+		f, err := os.Create("tmp/" + id + ".png")
+		if err != nil {
+			c.String(500, "internal error")
+			log.Println(err)
+			return
+		}
+
+		err = png.Encode(f, dest)
+		if err != nil {
+			c.String(500, "internal error")
+			log.Println(err)
+			return
+		}
+
+		c.Redirect(303, "/api/image/"+id)
+	})
+
+	r.GET("/api/image/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		c.Header("Content-Disposition", "attachment; filename=combined.png")
+		c.File("tmp/" + id + ".png")
+	})
+
+	r.Run()
 }
